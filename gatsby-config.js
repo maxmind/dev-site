dc0f9790660b5b0cf888e58575673b248f378c4b
Wes Rice <wrice@maxmind.com>
2020-04-12 22:56:05 -0400

    Add subresource integrity hashes to static files

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
       },
       resolve: 'gatsby-plugin-manifest',
     },
+    'gatsby-plugin-sri',
     // This (optional) plugin enables Progressive Web App + Offline
     // functionality. To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
