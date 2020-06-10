3261abfbb7227b8e7e87125b04450229cf160f14
Kevin Centeno <kevin@oldeyorke.ca>
2020-06-10 15:52:09 -0400

    Set the render order languages in CodeSet

--- gatsby/src/components/CodeSet.tsx
+++ gatsby/src/components/CodeSet.tsx
@@ -2,50 +2,58 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import { Store } from '../store';
-
 import styles from './CodeSet.module.scss';
 
-const getHumanReadable = (className: string): string  => {
-  const humanReadable = new Map([
-    [
-      'language-c',
-      'C',
-    ],
-    [
-      'language-java',
-      'Java',
-    ],
-    [
-      'language-javascript',
-      'JS',
-    ],
-    [
-      'language-json',
-      'JSON',
-    ],
-    [
-      'language-php',
-      'PHP',
-    ],
-    [
-      'language-python',
-      'Python',
-    ],
-    [
-      'language-ruby',
-      'Ruby',
-    ],
-    [
-      'language-sharp',
-      '.NET',
-    ],
-    [
-      'language-typescript',
-      'TypeScript',
-    ],
-  ]);
+// This defines both the human-readable version of the language class, and the
+// render order of the button selector.
+// If the language is not found in the Map, it is rendered first.
+// If multiple languages are not found in the Map, they are all rendered first
+// in order of appearance.
+const languageMap = new Map([
+  [
+    'language-c',
+    'C',
+  ],
+  [
+    'language-java',
+    'Java',
+  ],
+  [
+    'language-javascript',
+    'JS',
+  ],
+  [
+    'language-json',
+    'JSON',
+  ],
+  [
+    'language-php',
+    'PHP',
+  ],
+  [
+    'language-python',
+    'Python',
+  ],
+  [
+    'language-ruby',
+    'Ruby',
+  ],
+  [
+    'language-sharp',
+    '.NET',
+  ],
+  [
+    'language-typescript',
+    'TypeScript',
+  ],
+]);
 
-  return humanReadable.get(className) || className.replace('language-', '');
+const languageArray = [
+  ...languageMap.entries(),
+];
+
+const getHumanReadable = (className: string): string  => {
+  return languageMap.get(className) || className.replace('language-', '');
 };
 
 const CodeSet: React.FC = (props) => {
@@ -55,15 +63,35 @@ const CodeSet: React.FC = (props) => {
     saveActiveLanguage,
   ] = React.useState(context.selectedLanguage);
 
-  const languages = React.Children.map(props.children, child => {
+  const orderedChildren = React.Children.toArray(props.children).sort((a,b) => {
+    if (React.isValidElement(a) && React.isValidElement(b)) {
+      const indexA = languageArray
+        .findIndex(element => element[0] === a.props.children.props.className);
+      const indexB = languageArray
+        .findIndex(element => element[0] === b.props.children.props.className);
+
+      if (indexA - indexB > 0) {
+        return 1;
+      }
+
+      if (indexA - indexB < 0) {
+        return -1;
+      }
+    }
+
+    return -1;
+  });
+
+  const languages = React.Children.map(orderedChildren, child => {
     if (React.isValidElement(child)) {
       return child.props.children.props.className;
     }
   });
 
+
   // If the codeset contains the context language, display it.
   // Otherwise, don't change what is being displayed.
-  // Default to displaying the first language in the codeset.
+  // Default to displaying the first language in orderedChildren.
   React.useEffect(() => {
     if (languages) {
       if (languages.includes(context.selectedLanguage) &&
@@ -78,13 +106,17 @@ const CodeSet: React.FC = (props) => {
       // default to the first child
       saveActiveLanguage(languages[0]);
     }
-  });
+  }, [
+    languages,
+    context.selectedLanguage,
+    activeLanguage,
+  ]);
 
   return (
     <div>
       <nav>
         {
-          React.Children.map(props.children, child => {
+          React.Children.map(orderedChildren, child => {
             if (React.isValidElement(child)) {
               const className = child.props.children.props.className;
               return (
@@ -108,7 +140,7 @@ const CodeSet: React.FC = (props) => {
         }
       </nav>
       {
-        React.Children.map(props.children, child => {
+        React.Children.map(orderedChildren, child => {
           if (React.isValidElement(child)) {
             if (child.props.children.props.className ===
               activeLanguage) {
