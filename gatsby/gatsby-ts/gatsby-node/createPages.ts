39cae6a42c53ac8ee89a2d820f06f1f2c0052436
Wes Rice <wrice@maxmind.com>
2020-07-07 12:25:22 -0400

    Remove dead code

--- gatsby/gatsby-ts/gatsby-node/createPages.ts
+++ gatsby/gatsby-ts/gatsby-node/createPages.ts
@@ -1,11 +1,9 @@
 import { CreatePagesArgs, GatsbyNode } from 'gatsby';
 
 import createMdxPages from './pages/createMdxPages';
-// import createOpenApiPages from './pages/createOpenApiPages';
 
 export const createPages: GatsbyNode['createPages'] = async (
   props: CreatePagesArgs
 ) => {
   await createMdxPages(props);
-  // await createOpenApiPages(props);
 };
--- gatsby/gatsby-ts/gatsby-node/onPostBuild.ts
+++ gatsby/gatsby-ts/gatsby-node/onPostBuild.ts
@@ -1,5 +1,3 @@
-/* eslint-disable security/detect-non-literal-fs-filename */
-/* eslint-disable filenames/match-exported */
 import fs from 'fs';
 import { GatsbyNode } from 'gatsby';
 import path from 'path';
@@ -7,6 +5,7 @@ import path from 'path';
 import specs from '../../src/specs';
 
 export const onPreBuild: GatsbyNode['onPreInit'] = async () => {
+  // eslint-disable-next-line security/detect-non-literal-fs-filename
   fs.writeFileSync(
     path.resolve(`${__dirname}../../../static/specs/minfraud.json`),
     JSON.stringify(specs.minFraud.getSpec()),
