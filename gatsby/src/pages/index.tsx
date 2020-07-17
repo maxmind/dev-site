fe280ce1ef4db25d4080e31bc1892aa0808fed97
Wes Rice <wrice@maxmind.com>
2020-07-17 15:20:01 -0400

    Remove dead code

--- gatsby/src/pages/index.tsx
+++ gatsby/src/pages/index.tsx
@@ -1,21 +1,12 @@
-import { Link } from 'gatsby';
 import React from 'react';
 
 import Layout from '../components/Layout';
-import Image from '../components/Mdx/Img';
 
 const IndexPage: React.FC = () => (
   <Layout
     title="Home"
   >
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Image />
-    <Link
-      to="/page-2/"
-    >
-      Go to page 2
-    </Link>
+    Hello world.
   </Layout>
 );
 
