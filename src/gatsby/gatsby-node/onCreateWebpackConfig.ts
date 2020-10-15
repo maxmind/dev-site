7152434496bc54e2e8a2b0c0325f6005b00145ed
Wes Rice <wrice@maxmind.com>
2020-10-14 22:37:37 -0400

    Integrate Remark linting with Webpack for feedback when using `yarn develop`

--- src/gatsby/gatsby-node/onCreateWebpackConfig.ts
+++ src/gatsby/gatsby-node/onCreateWebpackConfig.ts
@@ -1,4 +1,6 @@
+import { exec } from 'child_process';
 import { CreateWebpackConfigArgs, GatsbyNode } from 'gatsby';
+import reporter from 'gatsby-cli/lib/reporter';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const StylelintPlugin = require('stylelint-webpack-plugin');
@@ -31,6 +33,26 @@ export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig']= (
           files: 'src/**/*.s(a|c)ss',
           fix: true,
         }),
+        {
+          apply: (compiler: any) => {
+            compiler.hooks.watchRun.tapAsync(
+              'RemarkLint',
+              (_: unknown, next: () => void) => exec(
+                'remark . --ext mdx --quiet',
+                (_: unknown, stdout: string, stderr: string) => {
+                  if (stdout) {
+                    reporter.info(stdout);
+                  }
+
+                  if (stderr) {
+                    reporter.warn(`RemarkLink error:\n${stderr}`);
+                  }
+
+                  next();
+                }
+              ));
+          },
+        },
       ],
     });
   }
