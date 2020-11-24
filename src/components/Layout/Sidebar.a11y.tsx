025d290f368ce8a878c57c9cd605232e91aca513
Wes Rice <wrice@maxmind.com>
2020-11-23 19:30:09 -0500

    Enable sidebar a11y test

--- src/components/Layout/Sidebar.a11y.tsx
+++ src/components/Layout/Sidebar.a11y.tsx
@@ -2,8 +2,6 @@ import 'jest-pa11y/build/extendExpect';
 
 import { useLocation } from '@reach/router';
 import { mount  } from 'enzyme';
-import { configurePa11y } from 'jest-pa11y';
-import { recorders } from 'jest-style-transformer-utils';
 import * as React from 'react';
 
 import Sidebar from './Sidebar';
@@ -16,19 +14,10 @@ import Sidebar from './Sidebar';
   state: {},
 });
 
-xdescribe('Sidebar', () => {
+describe('Sidebar', () => {
   it('has no Pa11y violations', async () => {
-    const pa11y = configurePa11y();
-
     const component = mount(<Sidebar />);
-
-    const html = `
-      <style>${recorders.styles.get()}</style>
-      ${component.html()}
-    `;
-
-    const results = await pa11y(html);
-
+    const results = await pa11y(component);
     expect(results).toHaveNoPa11yViolations();
   });
 });
