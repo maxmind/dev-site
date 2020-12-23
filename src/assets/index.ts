d19966441a8180376d88beae022b3a4c7794ddad
Wes Rice <wrice@maxmind.com>
2020-12-23 12:13:25 -0500

    Update product icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,11 +1,15 @@
 import * as React from 'react';
 
+import DatabaseIcon from './svgs/database-icon.svg';
 import GeoIP2Icon from './svgs/geoip2-icon.svg';
 import GeoLite2Icon from './svgs/geolite2-icon.svg';
 import MinFraudIcon from './svgs/minfraud-icon.svg';
+import WebServiceIcon from './svgs/web-service-icon.svg';
 
 export const Icons: Record<string, React.FC<React.SVGProps<SVGSVGElement>>> = {
+  DatabaseIcon,
   GeoIP2Icon,
   GeoLite2Icon,
   MinFraudIcon,
+  WebServiceIcon,
 };
