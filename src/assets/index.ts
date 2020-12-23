ca8b47ab3950aa03c29c11bae64ceb97f2213560
Wes Rice <wrice@maxmind.com>
2020-12-23 14:54:12 -0500

    Use Font Awesome icons for databases and web services

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import {
+  FaCloud as WebServiceIcon,
+  FaDatabase as DatabaseIcon,
+} from 'react-icons/fa';
 
-import DatabaseIcon from './svgs/database-icon.svg';
 import GeoIP2Icon from './svgs/geoip2-icon.svg';
 import GeoLite2Icon from './svgs/geolite2-icon.svg';
 import MinFraudIcon from './svgs/minfraud-icon.svg';
-import WebServiceIcon from './svgs/web-service-icon.svg';
 
 export const Icons: Record<string, React.FC<React.SVGProps<SVGSVGElement>>> = {
   DatabaseIcon,
