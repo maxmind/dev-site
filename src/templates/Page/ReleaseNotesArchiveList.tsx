d0850df2f1ff6bf58a6a2302d852f5ee049bd51c
Kevin Centeno <kevin@oldeyorke.ca>
2021-02-26 15:06:42 -0500

    Migrate geoip2 release notes

--- src/templates/Page/ReleaseNotesArchiveList.tsx
+++ src/templates/Page/ReleaseNotesArchiveList.tsx
@@ -10,7 +10,16 @@ export interface IReleaseNotesArchiveList {
 }
 
 const years: Record<ProductSlug, number[]> = {
-  geoip: [],
+  geoip: [
+    2020,
+    2019,
+    2018,
+    2017,
+    2016,
+    2015,
+    2014,
+    2013,
+  ],
   minfraud: [
     2020,
     2019,
--- test/regressions.spec.ts
+++ test/regressions.spec.ts
@@ -1,15 +1,24 @@
 import fetch from 'node-fetch';
 
 describe('regression tests', () => {
+  it('https://dev.maxmind.com/geoip/geoip2/release-notes/ regression test',
+    async () => {
+      const res =
+        await fetch('https://dev.maxmind.com/geoip/geoip2/release-notes/');
+      expect(await res.text()).toMatchSnapshot();
+    });
+
   it('https://dev.maxmind.com/minfraud/ regression test', async () => {
     const res = await fetch('https://dev.maxmind.com/minfraud/');
     expect(await res.text()).toMatchSnapshot();
   });
 
-  it('https://dev.maxmind.com/minfraud/release-notes/ regression test', async () => {
-    const res = await fetch('https://dev.maxmind.com/minfraud/release-notes/');
-    expect(await res.text()).toMatchSnapshot();
-  });
+  it('https://dev.maxmind.com/minfraud/release-notes/ regression test',
+    async () => {
+      const res =
+        await fetch('https://dev.maxmind.com/minfraud/release-notes/');
+      expect(await res.text()).toMatchSnapshot();
+    });
 });
 
 
