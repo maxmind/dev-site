2c89d4fef5190debe28eb3924a6cec570c99f1d2
Wes Rice <wrice@maxmind.com>
2021-05-23 22:17:24 -0400

    Add 404 design

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import * as React from 'react';
+import { FaRegSurprise as O } from 'react-icons/fa';
 
 import Layout from '../components/Layout/Layout';
+import { p as P } from '../components/Mdx';
+
+import * as styles from './FourOhFour.module.scss';
 
 const NotFoundPage: React.FC = () => (
   <Layout
+    hasSidebar={false}
     title="404: Not found"
   >
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div
+      aria-label="404"
+      className={styles.fourOhFour}
+      role="img"
+    >
+      4
+      <O />
+      4
+    </div>
+    <P
+      className={styles.message}
+    >
+      It seems we can’t find what you’re looking for. Perhaps searching can
+      help.
+    </P>
   </Layout>
 );
 
