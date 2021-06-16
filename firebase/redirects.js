8b39016e7e4e97ac1c035d7e80ed1da3e6e70ee6
Wes Rice <wrice@maxmind.com>
2021-06-16 13:51:03 -0400

    Add missing redirects to resolve unfound static files

--- firebase/redirects.js
+++ firebase/redirects.js
@@ -5,19 +5,120 @@
  * to make it a little easier to determine existing redirects.
  */
 
-module.exports = [
+const staticFileRedirects = [
   {
-    destination: 'https: //support.maxmind.com/',
+    destination: '/static/GeoIP2-Enterprise-CSV_Example.zip',
+    source: '/GeoIP2-Enterprise-CSV_Example.zip',
+    type: 301,
+  },
+  {
+    destination: '/static/svg/bg-pattern.svg',
+    source: '/assets/svg/bg-pattern.svg',
+    type: 301,
+  },
+  {
+    destination: '/static/csv/fips-iso-map.csv',
+    source: '/fips-iso-map.csv',
+    type: 301,
+  },
+  {
+    destination: '/static/geoip-test-databases.tar.gz',
+    source: '/geoip-test-databases.tar.gz',
+    type: 301,
+  },
+  {
+    destination: '/static/checksum.txt',
+    source: '/checksum.txt',
+    type: 301,
+  },
+  {
+    destination: '/static/csv/:1',
+    regex: '^\\/csv-files\\/(.*)$',
+    type: 301,
+  },
+  {
+    destination: '/static/csv/codes/maxmind/eu_country_list.csv',
+    source: '/static/csv/codes/eu_country_list.csv',
+    type: 301,
+  },
+  {
+    destination: '/static/csv/codes/maxmind/ap_country_list.csv',
+    source: '/static/csv/codes/ap_country_list.csv',
+    type: 301,
+  },
+  {
+    destination: '/static/csv/:1',
+    regex: '^\\/csv-files\\/(.*)$',
+    type: 301,
+  },
+  {
+    destination: '/static/csv/:1',
+    regex: '^\\/csv\\/(.*)$',
+    type: 301,
+  },
+  {
+    destination: '/static/pdf/:1',
+    regex: '^\\/static\\/pdfs\\/(.*)$',
+    type: 301,
+  },
+  {
+    destination: '/static/wsdl/:1',
+    regex: '^\\/(minfraud-soap-.*\\.wsdl)$',
+    type: 301,
+  },
+  {
+    destination: 'https://www.maxmind.com/download/geoip/misc/region_codes.csv',
+    source: '/static/maxmind-region-codes.csv',
+    type: 301,
+  },
+
+  // WordPress Uploads
+  {
+    destination: '/static/csv/:1',
+    regex: '^\\/20\\d\\d\\/\\d\\d\\/(GeoNames-Monthly-Diff-Report-.*)$',
+    type: 301,
+  },
+  {
+    destination: '/static/csv/fips-iso-map.csv',
+    source: '/2020/06/fips-iso-map.csv',
+    type: 301,
+  },
+  {
+    destination: '/static/GeoIP2-Enterprise-CSV_Example.zip',
+    source: '/2015/07/GeoIP2-Enterprise-CSV_Example.zip',
+    type: 301,
+  },
+  {
+    destination: '/static/GeoIP2-Enterprise-CSV_Example1.zip',
+    source: '/2015/07/GeoIP2-Enterprise-CSV_Example1.zip',
+    type: 301,
+  },
+  {
+    destination: '/static/GeoIP2-User-Count-CSV_Example.zip',
+    source: '/2017/10/GeoIP2-User-Count-CSV_Example.zip',
+    type: 301,
+  },
+  {
+    destination: '/static/csv/fips-iso-map.csv',
+    source: '/2020/06/fips-iso-map.csv',
+    type: 301,
+  },
+];
+
+module.exports = [
+  ...staticFileRedirects,
+  {
+    destination: 'https://support.maxmind.com/',
     source: '/faq',
     type: 302,
   },
   {
-    destination: 'https: //support.maxmind.com/',
+    destination: 'https://support.maxmind.com/',
     source: '/faq/*',
     type: 302,
   },
   {
-    destination: 'https: //support.maxmind.com/',
+    destination: 'https://support.maxmind.com/',
     source: '/faq/*',
     type: 302,
   },
@@ -217,7 +318,7 @@ module.exports = [
     type: 302,
   },
   {
-    destination: 'https: //github.com/maxmind/geoip-api-mod_geoip2',
+    destination: 'https://github.com/maxmind/geoip-api-mod_geoip2',
     source: '/geoip/legacy/mod_geoip2',
     type: 302,
   },
@@ -232,7 +333,7 @@ module.exports = [
     type: 302,
   },
   {
-    destination: 'https: //github.com/maxmind/MaxMind-DB/blob/main/MaxMind-DB-spec.md',
+    destination: 'https://github.com/maxmind/MaxMind-DB/blob/main/MaxMind-DB-spec.md',
     source: '/maxmind-db',
     type: 302,
   },
@@ -319,16 +420,6 @@ module.exports = [
   {
     destination: '/minfraud/proxy-detection',
     source: '/proxy-detection',
-    type: 302,
-  },
-  {
-    destination: '/csv-files/: 1',
-    regex: '^\\/static\\/csv\\/(.*)$',
-    type: 301,
-  },
-  {
-    destination: 'http: //www.maxmind.com/download/geoip/misc/region_codes.csv',
-    source: '/static/maxmind-region-codes.csv',
     type: 302,
   },
 ];
