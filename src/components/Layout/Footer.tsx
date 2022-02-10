15325e64f4705d0b46abf3e55b6846028ec3bb2f
Dallas Hogan <dhogan@maxmind.com>
2022-02-10 08:16:58 -0500

    updating footer for new geoip corrections page

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -154,7 +154,7 @@ const Footer: React.FC<React.HTMLProps<HTMLDivElement>> = (props) => {
           </a>
           <a
             className={styles.link}
-            href="https://support.maxmind.com/hc/en-us/articles/4408252036123-GeoIP-Corrections"
+            href="https://www.maxmind.com/en/geoip-data-correction-request"
           >
             GeoIP Data Correction Request
           </a>
