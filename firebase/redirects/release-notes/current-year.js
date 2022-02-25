b0029c7f3b5d264c5038743e1d0744d69a72dd2c
Dallas Hogan <dhogan@maxmind.com>
2022-02-25 17:38:21 -0500

    Add current year variable

--- firebase/redirects/release-notes/current-year.js
+++ firebase/redirects/release-notes/current-year.js
@@ -1,11 +1,14 @@
+const now = new Date();
+const year = now.getUTCFullYear();
+
 module.exports = [
   {
-    destination: '/geoip/release-notes/2022',
+    destination: `/geoip/release-notes/${year}`,
     source: '/geoip/release-notes',
     type: 302,
   },
   {
-    destination: '/minfraud/release-notes/2022',
+    destination: `/minfraud/release-notes/${year}`,
     source: '/minfraud/release-notes',
     type: 302,
   },
