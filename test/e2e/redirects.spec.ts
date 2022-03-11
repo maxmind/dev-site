c88a7e1919812dc068e1ec98a2d89256d3a22c9c
Gregory Oschwald <goschwald@maxmind.com>
2022-03-11 15:18:30 -0800

    Simplify fetching

--- test/e2e/redirects.spec.ts
+++ test/e2e/redirects.spec.ts
@@ -18,16 +18,13 @@ describe('Redirects', () => {
     it(
       `${url} matches checksum`,
       async () => {
-        const result = await fetch(url)
-          .then((response) => response.blob())
-          .then((blob: any) => new Promise((resolve, reject) => {
-            const hash = crypto.createHash('sha1');
-            const stream = blob.stream();
-            stream.on('error', (err: any) => reject(err));
-            stream.on('data', (chunk: any) => hash.update(chunk));
-            stream.on('end', () => resolve(hash.digest('hex')));
-          }));
-        expect(result).toBe(checksum);
+        const response = await fetch(url);
+        const buffer = await response.arrayBuffer();
+        const view = new Uint8Array(buffer);
+        const hash = crypto.createHash('sha1');
+
+        hash.update(view);
+        expect(hash.digest('hex')).toBe(checksum);
       }
     );
   });
