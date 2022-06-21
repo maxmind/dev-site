7776003e7cd874f495c2a8f087eb57712aba2a8b
Dallas Hogan <dhogan@maxmind.com>
2022-06-21 10:12:41 -0400

    Add comment about hardcoding year for release notes pages

--- firebase/redirects/release-notes/current-year.js
+++ firebase/redirects/release-notes/current-year.js
@@ -1,13 +1,15 @@
-const year = 2022;
+// The year needs to be hardcoded. A page for the new year's
+// release notes won't exist until a new MDX file is created
+// in the /content/<product>/release-notes folder for that new year.
 
 module.exports = [
   {
-    destination: `/geoip/release-notes/${year}`,
+    destination: '/geoip/release-notes/2022',
     source: '/geoip/release-notes',
     type: 302,
   },
   {
-    destination: `/minfraud/release-notes/${year}`,
+    destination: '/minfraud/release-notes/2022',
     source: '/minfraud/release-notes',
     type: 302,
   },
--- src/gatsby/gatsby-config/index.ts
+++ src/gatsby/gatsby-config/index.ts
@@ -124,6 +124,9 @@ export default {
     {
       options: {
         feeds: [
+          // The year needs to be hardcoded. A page for the new year's
+          // release notes won't exist until a new MDX file is created
+          // in the /content/<product>/release-notes folder for that new year.
           createReleaseNotesFeed({
             description: 'Release notes for MaxMind\'s GeoIP2 product line',
             inputUrl: '/geoip/release-notes/2022',
