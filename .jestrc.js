5cabd923c3713130900ae13db8649191fee0eba0
Gregory Oschwald <goschwald@maxmind.com>
2022-07-07 10:06:30 -0700

    Add more modules to transformIgnorePatterns and run npm i

--- .jestrc.js
+++ .jestrc.js
@@ -27,6 +27,6 @@ module.exports = {
     '^.+\\.scss$': 'jest-style-transformer-sass-css-modules',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(data-uri-to-buffer|fetch-blob|formdata-polyfill|gatsby-link|gatsby-script|remark-html|node-fetch|typeface-montserrat)/)',
+    'node_modules/(?!(ccount|character-entities-html4|character-entities-legacy|comma-separated-tokens|data-uri-to-buffer|fetch-blob|formdata-polyfill|gatsby-link|gatsby-script|hast-util-is-element|hast-util-sanitize|hast-util-to-html|hast-util-whitespace|html-void-elements|mdast-util-definitions|mdast-util-to-hast|micromark-util-character|micromark-util-encode|micromark-util-sanitize-uri|node-fetch|property-information|remark-html|space-separated-tokens|stringify-entities|trim-lines|typeface-montserrat|unist-builder|unist-util-generated|unist-util-is|unist-util-position|unist-util-visit|unist-util-visit-parents)/)',
   ],
 };
