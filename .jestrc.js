2a11543659887c87360e0e99d6d3a809f325fefa
Dallas Hogan <dhogan@maxmind.com>
2022-07-07 15:20:03 -0400

    Add jest-scss-transform and remove jest-style-transformer-sass-css-modules

--- .jestrc.js
+++ .jestrc.js
@@ -6,7 +6,8 @@ module.exports = {
     '.+\\.(css|styl|less)$': 'identity-obj-proxy',
     // eslint-disable-next-line max-len
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/file-mock.ts',
-    '^gatsby-page-utils/(.*)$': 'gatsby-page-utils/dist/$1', // Workaround for https://github.com/facebook/jest/issues/9771
+    // eslint-disable-next-line max-len
+    '^gatsby-page-utils/(.*)$': 'gatsby-page-utils/$1', // Workaround for https://github.com/facebook/jest/issues/9771
   },
   setupFiles: [
     '<rootDir>/test/loadershim.tsx',
@@ -24,9 +25,10 @@ module.exports = {
   testURL: 'http://localhost',
   transform: {
     '^.+\\.[jt]s(x)?$': '<rootDir>/test/preprocess.js',
-    '^.+\\.scss$': 'jest-style-transformer-sass-css-modules',
+    '^.+\\.scss$': 'jest-scss-transform',
   },
   transformIgnorePatterns: [
+    // eslint-disable-next-line max-len
     'node_modules/(?!(data-uri-to-buffer|fetch-blob|formdata-polyfill|gatsby-link|gatsby-script|node-fetch|typeface-montserrat)/)',
   ],
 };
