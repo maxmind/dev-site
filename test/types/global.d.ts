f10099009f9e1fd63bc0a13d376115cefa80ee4d
Gregory Oschwald <goschwald@maxmind.com>
2022-07-20 07:33:10 -0700

    Remove unnecessary puppeteer config

--- test/types/global.d.ts
+++ test/types/global.d.ts
@@ -1,10 +1,6 @@
 import type { ReactWrapper } from 'enzyme';
-import { Browser, Page } from 'puppeteer';
 
 declare global {
-  const browser: Browser;
-  const page: Page;
-
   // eslint-disable-next-line @typescript-eslint/naming-convention
   interface Window {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
