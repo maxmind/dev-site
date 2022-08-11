2f20e207483b2f8511f0b02b40f9bbd585889231
Dallas Hogan <dallastar4@gmail.com>
2022-08-11 15:54:10 -0400

    Add type for Footnote

--- src/components/Schema/Footnote.tsx
+++ src/components/Schema/Footnote.tsx
@@ -3,7 +3,11 @@ import * as React from 'react';
 
 import * as styles from './Footnote.module.scss';
 
-const Footnote: React.FC = (props) => {
+interface IFootnote {
+  children: string;
+}
+
+const Footnote: React.FC<IFootnote> = (props) => {
   const { children } = props;
 
   return (
@@ -16,7 +20,7 @@ const Footnote: React.FC = (props) => {
 };
 
 Footnote.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.string.isRequired,
 };
 
 export default Footnote;
