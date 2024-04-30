f8417d6bbc8958e728623a613a57dcf9b13982ae
kevcenteno <kevin@oldeyorke.ca>
2024-04-30 11:37:10 -0400

    Init hugo and delete gatsby related files

--- test/preprocess.js
+++
@@ -1,8 +0,0 @@
-const babelJest = require('babel-jest');
-
-module.exports = babelJest.default.createTransformer({
-  presets: [
-    'babel-preset-gatsby',
-    '@babel/preset-typescript',
-  ],
-});
