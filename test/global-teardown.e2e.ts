ec9c4ac2f8ee588cc92378b29fa366be57c58011
kevcenteno <kevin@oldeyorke.ca>
2024-04-30 11:37:10 -0400

    Init hugo and delete gatsby related files

--- test/global-teardown.e2e.ts
+++
@@ -1,9 +0,0 @@
-import { teardown as teardownDevServer } from 'jest-dev-server';
-
-const targetUrl = process.env.E2E_TARGET_URL || 'http://localhost:5000';
-
-export default async (): Promise<void> => {
-  if (targetUrl === 'http://localhost:5000') {
-    await teardownDevServer();
-  }
-};
