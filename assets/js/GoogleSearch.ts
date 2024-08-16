c7bac17541e22db7047e08c28044c48a306898df
kevcenteno <kevin@oldeyorke.ca>
2024-08-16 14:29:24 -0400

    Address feedback

--- assets/js/GoogleSearch.ts
+++ assets/js/GoogleSearch.ts
@@ -23,16 +23,12 @@ export interface ISearchResults {
   };
 }
 
-const endpoint =
-  "https://www.googleapis.com/customsearch/v1/siterestrict?cx={cx}&key={key}";
-
 const cx = "cde039a7678700a13";
 const key = "AIzaSyAI4atAz3I5ujXCjoEXRvdwqcYn3AIsCA8";
-
-const url = endpoint.replace("{cx}", cx).replace("{key}", key);
+const url = `https://www.googleapis.com/customsearch/v1/siterestrict?cx=${cx}&key=${key} `;
 
 const GoogleSearch = async (
-  query: string | null,
+  query: string,
   startIndex: string | null
 ): Promise<ISearchResults> => {
   let requestUrl = `${url}&q=${query}`;
